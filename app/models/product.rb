350618907628f69c332e6778a9418c04627bb2bf
Kan Wu <wukan@avos.com>
2012-05-23 02:22:16 -0400

    a smarter cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
-  default_scope :order => 'title' 
+  default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item 
   
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :presence => true   
@@ -10,4 +12,15 @@ class Product < ActiveRecord::Base
     :with => %r(\.(gif|png|jpg)$)i,
     :message => 'must be a URL for GIF, JPG, PNG image',  
   } 
+  
+  private
+  
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
