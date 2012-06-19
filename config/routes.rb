40ed83bd899576ade4dc557de35f2b90da68429f
Kan Wu <wukan@avos.com>
2012-06-19 00:57:49 -0700

    Check out!

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Depot::Application.routes.draw do
 
   get "store/index"
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+  root to: 'store#index', as: 'store'
 
   # ...
   # The priority is based upon order of creation:
